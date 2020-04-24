aa66c73e3672f99c95b8ec711e24706d0ca6d885
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2020-04-25 00:38:45 +0300

    fix test imports

--- test/test_dbow.py
+++ test/test_dbow.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 
 import unittest
-import orb_descriptor
 import dbow
 import numpy as np
 
 
 class TestDBOW(unittest.TestCase):
     def test_weight(self):
-        descriptors = [orb_descriptor.ORB([0, 1, 0, 0, 1]),
-                       orb_descriptor.ORB([1, 1, 1, 1, 1]),
-                       orb_descriptor.ORB([0, 0, 0, 1, 0]),
-                       orb_descriptor.ORB([0, 1, 0, 0, 1])]
+        descriptors = [dbow.ORB([0, 1, 0, 0, 1]),
+                       dbow.ORB([1, 1, 1, 1, 1]),
+                       dbow.ORB([0, 0, 0, 1, 0]),
+                       dbow.ORB([0, 1, 0, 0, 1])]
         word = dbow.Word(descriptors)
         word.update_weight(4)
         self.assertEqual(word.weight, 0.0)
 
     def test_tree(self):
-        descriptors = [orb_descriptor.ORB([0, 1, 0, 0, 1]),
-                       orb_descriptor.ORB([1, 1, 1, 1, 1]),
-                       orb_descriptor.ORB([0, 0, 0, 1, 0]),
-                       orb_descriptor.ORB([0, 1, 0, 0, 1])]
+        descriptors = [dbow.ORB([0, 1, 0, 0, 1]),
+                       dbow.ORB([1, 1, 1, 1, 1]),
+                       dbow.ORB([0, 0, 0, 1, 0]),
+                       dbow.ORB([0, 1, 0, 0, 1])]
         root = dbow.Node(descriptors)
         words = dbow.initialize_tree(root, 4, 4)
         for i, word in enumerate(words):
--- test/test_descriptor.py
+++ test/test_descriptor.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 
 import unittest
-import orb_descriptor
+import dbow
 import numpy as np
 
 
 class TestORB(unittest.TestCase):
     def test_mean(self):
-        descriptors = [orb_descriptor.ORB([0, 1, 0, 0, 1]),
-                       orb_descriptor.ORB([1, 1, 1, 1, 1]),
-                       orb_descriptor.ORB([0, 0, 0, 1, 0]),
-                       orb_descriptor.ORB([0, 1, 0, 0, 1])]
+        descriptors = [dbow.ORB([0, 1, 0, 0, 1]),
+                       dbow.ORB([1, 1, 1, 1, 1]),
+                       dbow.ORB([0, 0, 0, 1, 0]),
+                       dbow.ORB([0, 1, 0, 0, 1])]
 
         self.assertEqual(
-                orb_descriptor.mean_value(descriptors),
-                orb_descriptor.ORB([0, 1, 0, 1, 1]))
+                dbow.mean_value(descriptors),
+                dbow.ORB([0, 1, 0, 1, 1]))
 
     def test_distance(self):
-        desc1 = orb_descriptor.ORB([0, 1, 0, 0, 1])
-        desc2 = orb_descriptor.ORB([1, 1, 1, 1, 1])
+        desc1 = dbow.ORB([0, 1, 0, 0, 1])
+        desc2 = dbow.ORB([1, 1, 1, 1, 1])
         self.assertEqual(desc1.distance(desc2), 3)
 
     def test_sum(self):
-        descriptors = [orb_descriptor.ORB([0, 1, 0, 0, 1]),
-                       orb_descriptor.ORB([1, 1, 1, 1, 1]),
-                       orb_descriptor.ORB([0, 0, 0, 1, 0]),
-                       orb_descriptor.ORB([0, 1, 0, 0, 1])]
+        descriptors = [dbow.ORB([0, 1, 0, 0, 1]),
+                       dbow.ORB([1, 1, 1, 1, 1]),
+                       dbow.ORB([0, 0, 0, 1, 0]),
+                       dbow.ORB([0, 1, 0, 0, 1])]
         np.allclose(np.sum(descriptors).features,
                     np.array([1, 3, 1, 2, 3]))
 
